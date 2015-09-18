709659bba3ffa1a11bf9be3e6c4cb2bbdf78fddf
Istomin Bohdan <sithobog@ymail.com>
2015-09-18 05:41:53 +0300

    add generation links to index

--- lib/seo/seo_parser.rb
+++ lib/seo/seo_parser.rb
@@ -27,14 +27,35 @@ module Seo
       links
     end
 
-    def create_file(site_url, headers, links)
-      _body = Slim::Template.new(File.expand_path('report.slim', "views/"), {}).render(self)
+    def prepare_site_url(site_url)
+        site_url.chop! if site_url[-1] == "/"
+        site_url.gsub!('/', '_') if site_url.include?("/")
       if site_url.include?("https")
         site_url = site_url[8..site_url.length-1]
       else
         site_url = site_url[7..site_url.length-1]
       end
-      File.write(File.expand_path("#{site_url}_#{Time.now.to_s}.html", "public/reports"),_body)
+    end
+
+    def create_file(site_url, headers, links)
+      _body = Slim::Template.new(File.expand_path('report.slim', "views/"), {}).render(self)
+      _ready_url = prepare_site_url(site_url)
+      _time = time_format
+      File.write(File.expand_path("#{_ready_url}_#{_time}.html", "public/reports"),_body)
+      add_link_to_index(_ready_url, _time)
+    end
+
+    def add_link_to_index(url,time)
+      open('views/index.slim', 'a') do |f|
+        f << "\r a href='../reports/#{url}_#{time}.html' #{url}\n"
+      end
+    end
+
+    def time_format
+      _time = Time.now.to_s
+      _time = _time.gsub(" ","_")
+      _time = _time.gsub(":", "-")
+      _time = _time[0..-7]
     end
 
   end
